e9690ceb99cfe7d2cebd4a8a7487f8a6b2678b5d
Jennifer <jsteiner@iastate.edu>
2014-04-02 19:43:49 -0400

    Test github commit
    Just added additional text to the index to test commit

--- index.php
+++ index.php
@@ -5,14 +5,13 @@ session_start();
 
 include 'includes/header.php'; 
 
-
 ?>
 
 
 <div class="colmask fullpage">
 	<div class="col1">
 
-		<h2>Find Business Hours Near You</h2>
+		<h2>Find Business Hours Near You TESTTESTTEST</h2>
 		<br />
 		<p>Wouldn’t it be great if every store you ever went to was always open 24 hours?  This would save all of us from having to rush to the doors of our local market before they lock up, or it could save us from driving to a store that’s already closed for the evening. Unfortunately, this utopia doesn’t exist and we’re left with some stores opened with limited hours during holidays, some closed on Mondays, while some close early on weekends, and hours always change during the Thanksgiving-Christmas rush - It’s memory overload!  To alleviate the processing power and memory you have to use to remember such trivial things - Business Hours is here to help!</p>
 
